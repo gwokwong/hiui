c29d5c191ae1e519c65a5c11197074dc2cf0706e
web2033 <github@web2033.com>
2021-01-29 08:39:54 +0200

    Remove obsolete 'shadow-outline' class

--- src/components/ButtonRepo.vue
+++ src/components/ButtonRepo.vue
@@ -3,7 +3,7 @@
     <a
       target="_blank"
       href="https://github.com/web2033/vite-vue3-tailwind-starter"
-      class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-indigo-500 focus:outline-none focus:shadow-outline"
+      class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-indigo-600 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-indigo-500 focus:outline-none"
       >Learn more</a
     >
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
           <div class="inline-flex rounded-md shadow">
             <router-link
               to="/about"
-              class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:shadow-outline"
+              class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none"
               >Next Page</router-link
             >
           </div>
