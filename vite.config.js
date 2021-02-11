a979acb194445c392c8f0a74aa2731954602d9cf
web2033 <github@web2033.com>
2021-02-11 17:59:00 +0200

    server.open = true

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-/** @type {import('vite').UserConfig} */
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default {
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
     },
+  },
+  server: {
+    // Automatically open the app in the browser on server start
+    open: true,
   },
 }
