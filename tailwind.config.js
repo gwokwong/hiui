b22d40eaefe7ef8c41252f4f0f4181fa9cdef5ca
web2033 <github@web2033.com>
2021-04-13 09:55:25 +0300

    use @types/tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
