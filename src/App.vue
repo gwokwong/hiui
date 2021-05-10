a04648fb5b9f398dc655c85c5ff5bbca20f5f977
Bogenbai Bayzharassov <thatoranzhevyy@gmail.com>
2021-05-11 00:01:30 +0600

    fix main tag

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <main>
+  <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
       <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }}</h1>
       </div>
     </header>
-
-    <router-view />
-  </main>
+    <main>
+      <router-view/>
+    </main>
+  </div>
 </template>
