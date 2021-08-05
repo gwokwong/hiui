3c8cc746a68c33a189ec6cd940c58a4bf9cdbc8c
web2033 <github@web2033.com>
2021-08-05 09:08:37 +0300

    Enable `Ref sugar 2` (experimantal) https://github.com/vuejs/rfcs/discussions/369

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      script: {
+        refSugar: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
