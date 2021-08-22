6b4bc61bdcde69a3fa799b7259345ac10488519c
web2033 <github@web2033.com>
2021-08-22 08:14:09 +0300

    Remove `refSugar` (implementation is not stable)

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    vue({
-      script: {
-        refSugar: true,
-      },
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
