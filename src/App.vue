8e4fce816ab79401df761fc56153d9ebf8441dcb
web2033 <github@web2033.com>
2021-08-31 15:19:18 +0300

    Add simple `counter`

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+let counter = ref(0)
+setInterval(() => {
+  counter.value++
+}, 1000)
+</script>
 <template>
   <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }}</h1>
+      <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1
+          @click="counter = 0"
+          class="text-3xl font-bold leading-tight text-gray-900"
+        >
+          {{ $route.meta.title }} / {{ counter }}
+        </h1>
       </div>
     </header>
     <main>
-      <router-view/>
+      <router-view />
     </main>
   </div>
 </template>
