3d64d5415f8572c7619816b58dffce98e8b9a1cd
web2033 <github@web2033.com>
2021-08-31 15:23:29 +0300

    More space

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import { ref } from 'vue'
 
 let counter = ref(0)
+
 setInterval(() => {
   counter.value++
 }, 1000)
 </script>
+
 <template>
   <div>
     <header class="bg-white shadow" v-if="$route.meta.title">
