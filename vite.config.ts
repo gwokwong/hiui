72b48a05d9420f34a0e6ba1909f6bf1cc66fd1db
DateTime <github@web2033.com>
2022-08-26 22:01:07 +0300

    changes for `unplugin-vue-router 0.2.0`

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import VueRouter from 'unplugin-vue-router/vite'
-import { VueRouterExports } from 'unplugin-vue-router'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 import Components from 'unplugin-vue-components/vite'
 import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
+    VueRouter({}),
     vue(),
-    VueRouter(),
     Components({ resolvers: [HeadlessUiResolver()] }),
     AutoImport({
-      imports: ['vue', '@vueuse/head', { 'vue-router/auto': VueRouterExports }],
+      imports: ['vue', '@vueuse/head', VueRouterAutoImports],
     }),
   ],
   server: {
