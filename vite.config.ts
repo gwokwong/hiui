9bee5bf1720a42bd280f29a623101f0e253f7781
Daniel Morrison <1013921026@qq.com>
2023-01-15 11:56:16 +0800

    [Feat] add element-plus dependency

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,16 @@ import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import Components from 'unplugin-vue-components/vite'
 import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
     VueRouter({}),
     vue(),
-    Components({ resolvers: [HeadlessUiResolver()] }),
+    Components({ resolvers: [HeadlessUiResolver(), ElementPlusResolver()] }),
     AutoImport({
       imports: ['vue', '@vueuse/head', VueRouterAutoImports],
+      resolvers: [ElementPlusResolver()]
     }),
   ],
   server: {
