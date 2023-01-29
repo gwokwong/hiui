4feaf6bbddd35a8005300a34b12acc1415c87939
Daniel Morrison <1013921026@qq.com>
2023-01-29 16:56:11 +0800

    feat: 设置页面基础 refactor: 顶部栏设置按钮跳转

--- src/lib/router/i18n/en_US.ts
+++ src/lib/router/i18n/en_US.ts
@@ -1,6 +1,7 @@
 export default {
     'routes.dashboard': 'Home',
     'routes.login': 'Sign in',
+    'routes.settings': 'Settings',
     'routes.vFormDesigner': 'VForm designer',
     'routes.notFound': '404 Page Not Found',
 }
--- src/lib/router/i18n/zh_CN.ts
+++ src/lib/router/i18n/zh_CN.ts
@@ -1,6 +1,7 @@
 export default {
     'routes.dashboard': '仪表盘',
     'routes.login': '登录',
+    'routes.settings': '设置',
     'routes.vFormDesigner': 'VForm设计器',
     'routes.notFound': '404页面不存在',
 }
